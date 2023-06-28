f603d84b95ed9fbffeae135b116c1a4bbf8d1bdf
Asa <asaforpresident@gmail.com>
2023-06-28 12:58:00 -0700

    initial commit

---
+++ src/main/java/strident/asasflora/AsasFloraDataGenerator.java
@@ -0,0 +1,11 @@
+package strident.asasflora;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AsasFloraDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
