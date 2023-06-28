b5be64172661b37c02e829e865c3cf0dbf6d56d0
Asa <asaforpresident@gmail.com>
2023-06-28 13:18:39 -0700

    client mod, json edits, readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Asa's Flora
+
+Mod for Minecraft 1.20.1 that adds a variety of new plants, trees, crops, and fungi, both fictional and nonfictional
+
+Flora to be Added:
+
+- Spider Lily
+
+- Durian
---
+++ src/main/java/strident/asasflora/AsasFloraClient.java
@@ -0,0 +1,10 @@
+package strident.asasflora;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AsasFloraClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
